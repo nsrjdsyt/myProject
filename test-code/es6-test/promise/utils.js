5b0ee4c52aec7c1e0e9ed1f7cb5091a353853627
nsrjdsyt <taozhenwei0202@126.com>
2018-02-09 16:34:38 +0800

    test code

--- test-code/es6-test/promise/utils.js
+++ test-code/es6-test/promise/utils.js
@@ -34,55 +34,55 @@ export const getJSON = (url) => {
 
 
 //example:
-// getJSON("/posts.json").then(function(json) {
+// getJSON("/posts.json").then((json) => {
 //   console.log('Contents: ' + json);
 // }, function(error) {
 //   console.error('出错了', error);
 // });
 
-export const jsonp = (() => {
-  let count = 0
-  let body = document.body
-
-  const paramsToStr = (params) => {
-    let temp = []
-    for (let key in params) {
-      if (params.hasOwnProperty(key)) {
-        temp.push(`${key}=${params[key]}`)
-      }
-    }
-
-    return temp.join('&')
-  }
-
-  return (url, options, func) => {
-    if (typeof options === 'function') {
-      func = options
-      options = {}
-    }
-
-    let opts = {
-      callback: 'callback',
-      params: {}
-    }
-
-    const {callback, params} = Object.assign(opts, options)
-
-    const id = 'jsonp_' + count++ + Date.now()
-
-    params[callback] = id
-
-    url += (~url.indexOf('?') ? '&' : '?') + paramsToStr(params)
-
-    let script = document.createElement('script')
-
-    window[id] = data => {
-      func(data)
-      body.removeChild(script)
-      window[id] = null
-    }
-
-    script.src = url
-    body.appendChild(script)
-  }
-})()
+// export const jsonp = (() => {
+//   let count = 0
+//   let body = document.body
+//
+//   const paramsToStr = (params) => {
+//     let temp = []
+//     for (let key in params) {
+//       if (params.hasOwnProperty(key)) {
+//         temp.push(`${key}=${params[key]}`)
+//       }
+//     }
+//
+//     return temp.join('&')
+//   }
+//
+//   return (url, options, func) => {
+//     if (typeof options === 'function') {
+//       func = options
+//       options = {}
+//     }
+//
+//     let opts = {
+//       callback: 'callback',
+//       params: {}
+//     }
+//
+//     const {callback, params} = Object.assign(opts, options)
+//
+//     const id = 'jsonp_' + count++ + Date.now()
+//
+//     params[callback] = id
+//
+//     url += (~url.indexOf('?') ? '&' : '?') + paramsToStr(params)
+//
+//     let script = document.createElement('script')
+//
+//     window[id] = data => {
+//       func(data)
+//       body.removeChild(script)
+//       window[id] = null
+//     }
+//
+//     script.src = url
+//     body.appendChild(script)
+//   }
+// })()
