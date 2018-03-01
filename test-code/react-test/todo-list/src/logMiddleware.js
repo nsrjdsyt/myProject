8ad73e486fd5110388ffbcb172b04a5d6a2b0d3d
nsrjdsyt <taozhenwei0202@126.com>
2018-03-01 16:47:15 +0800

    add react-transition-group

--- test-code/react-test/todo-list/src/logMiddleware.js
+++ test-code/react-test/todo-list/src/logMiddleware.js
@@ -1,7 +1,7 @@
 export default ({dispatch, getState}) => { //中间件 在 dispatch(action) action 到达reducer前执行； 打印log的中间件， 不论同步action 还是异步action 都会执行
   return (next) => {
     return (action) => {
-      if(typeof action == 'function'){     //针对异步action
+      if(typeof action === 'function'){     //针对异步action
         console.log("logMiddleware ", action, getState())
         return action(dispatch)
       }
--- test-code/react-test/todo-list/src/todos/views/todoList.js
+++ test-code/react-test/todo-list/src/todos/views/todoList.js
@@ -2,18 +2,22 @@ import React from 'react'
 import TodoItem from './todoItem'
 import {connect} from 'react-redux'
 import {selectVisibleTodos} from './../selector'
+import TransitionGroup from 'react-transition-group/CSSTransitionGroup'
+import './todoList.css'
 
 const TodoList = ({todos, onAdd, onDelete, onToggle}) => {
   return (
     <ul>
-      {todos.map(item => (
-        <TodoItem
-          key={item.id}
-          id={item.id}
-          text={item.text}
-          completed={item.completed}
-        />
-      ))}
+      <TransitionGroup transitionName="fade" transitionEnterTimeout={500} transitionLeaveTimeout={200}>
+        {todos.map(item => (
+          <TodoItem
+            key={item.id}
+            id={item.id}
+            text={item.text}
+            completed={item.completed}
+          />
+        ))}
+      </TransitionGroup>
     </ul>
   )
 }
